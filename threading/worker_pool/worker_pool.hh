4f4524e726c9805937b34935c11edced6e35394b
IlYAN-FISHERMAN <Ilkay.yanar71@proton.me>
2026-02-26 15:35:45 +0100

    update

--- threading/worker_pool/worker_pool.hh
+++ threading/worker_pool/worker_pool.hh
@@ -6,7 +6,7 @@
 /*   By: ilyanar <ilyanar@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 18:50:12 by ilyanar           #+#    #+#             */
-/*   Updated: 2026/02/26 14:10:54 by ilyanar          ###   LAUSANNE.ch       */
+/*   Updated: 2026/02/26 14:16:53 by ilyanar          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
