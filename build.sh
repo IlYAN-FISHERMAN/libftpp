2b91ab2090a3a092844e38eaeade89c85dbac22e
Ilkay Yanar <ilyanar@c1r10s2.42lausanne.ch>
2026-05-19 13:13:08 +0200

    test on macos the builder

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ "$1" == "run" ];
+if [ "$1" = "run" ];
 then
 	cmake -B build -S .  -DBUILD_CLIENT=OFF -DBUILD_LIB=OFF -DBUILD_BINARY=ON
-elif [ "$1" == "install" ];
+elif [ "$1" = "install" ];
 then
 	cmake -B build -S .  -DBUILD_CLIENT=OFF -DBUILD_LIB=ON -DBUILD_BINARY=OFF
 	sudo cmake --install build --prefix /usr/local
-elif [ "$1" == "clear" ];
+elif [ "$1" = "clear" ];
 then
   rm -rf ./build/
 else
